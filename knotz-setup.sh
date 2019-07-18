1fbabbffc288cc9d07ad5a8e4982dd51588d2eeb
Fabian Fabian <170145+afknapping@users.noreply.github.com>
2019-07-18 06:16:54 +0200

    Update knotz-setup.sh

--- knotz-setup.sh
+++ knotz-setup.sh
@@ -12,3 +12,8 @@ source .bash_profile
 
 # rm .bash_profile
 # rm -rf knotz-setup/
+
+### ROMS
+
+# git daemon --verbose --export-all --base-path=.git --reuseaddr --strict-paths .git/
+# git clone git://172.20.10.2/ roms_best_of
